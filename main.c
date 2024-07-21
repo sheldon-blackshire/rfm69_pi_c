6cda9097b777848f0942be944dced228555d2b3a
SheldonBlackshire <sheldon.blackshire@celltracktech.com>
2024-07-21 16:41:25 -0400

    attempting first rx

--- main.c
+++ main.c
@@ -15,6 +15,11 @@
 #define GPIO_RFM_MOSI   10
 #define GPIO_RFM_SCK    11
 
+
+void main_on_rfm69_rx(struct rfm69_device* dev, uint8_t* data, uint16_t size, int16_t rssi) {
+    printf("%02x%02x%02x%02x,%d\n", data[0], data[1], data[2], data[3], rssi);
+}
+
 int main() {
     printf("rfm69.c -> main()\n");
 
@@ -29,6 +34,23 @@ int main() {
     if(dev == NULL){
         printf("rfm69_init fail\n");
     }
+
+    rfm69_receive(dev, &(struct rfm69_config) {
+        .center_freq_hz = 434000000,
+        .br = RFM69_BITRATE_25_KBPS,
+        .dcc = RFM69_DC_CANCEL_PRXBW_4,
+        .modulation = RFM69_MODULATION_FSK,
+        .mode = RFM69_DATA_MODE_PACKET,
+        .shaping = RFM69_MODULATION_SHAPING_FSK_NONE,
+        .sync_words = { 0xd3, 0x91 },
+        .sync_count = 2,
+        .preamble_length = 2,
+        .payload_length = 4,
+        .fsk = {
+            .freq_deviation_hz = 25000,
+            .bw = RFM69_BW_FSK_50_0_khz
+        }        
+    }, main_on_rfm69_rx);
 
     return 0;
 }
